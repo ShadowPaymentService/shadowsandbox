81d513886b11eb0092f557ca37a5dd29434135be
ShadowPaymentService <shadowcraftofficial272@gmail.com>
2026-04-30 03:11:37 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js
+import { initializeApp } from "https://gstatic.com";
+import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "https://gstatic.com";
+import { getFirestore } from "https://gstatic.com";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDpxt8iUQBKODu1VvP4hPHl_LyJQKKvZSQ",
-  authDomain: "codesandbox-8272e.firebaseapp.com",
-  projectId: "codesandbox-8272e",
-  storageBucket: "codesandbox-8272e.firebasestorage.app",
-  messagingSenderId: "58218651704",
-  appId: "1:58218651704:web:8f42123496a37c768946cd",
-  measurementId: "G-GV7W182MBF"
+    apiKey: "AIzaSyDpxt8iUQBKODu1VvP4hPHl_LyJQKKvZSQ",
+    authDomain: "codesandbox-8272e.firebaseapp.com",
+    projectId: "codesandbox-8272e",
+    storageBucket: "codesandbox-8272e.firebasestorage.app",
+    messagingSenderId: "58218651704",
+    appId: "1:58218651704:web:8f42123496a37c768946cd"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const googleProvider = new GoogleAuthProvider();
+export const githubProvider = new GithubAuthProvider();
